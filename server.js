cbc92be1eaf6baee0f428d1002458a32b1c71066
8972-amick <khadkaamick2@gmail.com>
2025-08-29 17:28:09 -0700

    backend completed

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const path = require("path");
+const morgan = require("morgan");
+
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const allowedOrigins = ["http://localhost:5173", "http://localhost:8081"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true); // allow mobile/native apps (no origin)
+      if (allowedOrigins.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+      return callback(null, true);
+    },
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+const authRoutes = require("./routes/authRoutes");
+const bookRoutes = require("./routes/bookRoutes");
+const borrowRoutes = require("./routes/borrowRoutes");
+
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
+app.use("/api/borrow", borrowRoutes);
+
+// MongoDB Connection
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("✅ MongoDB Connected");
+    app.listen(5000, "0.0.0.0", () =>
+      console.log("🚀 Server running on port 5000")
+    );
+  })
+  .catch((err) => {
+    console.error("❌ MongoDB connection failed:");
+    console.error(err.message || err);
+  });
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
